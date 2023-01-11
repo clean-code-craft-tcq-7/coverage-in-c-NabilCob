141c8a6b787260302ddb7e8293ef96a3eb07781f
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-11 08:58:31 +0530

    Update test-alerts.cpp
    Test

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,7 +3,7 @@
 #include "test/catch.hpp"
 #include "alert.h"
 
-TEST_CASE("Test breach level for passive cooling") {
+TEST_CASE("Test breach level") {
   BatteryCharacter batteryChar;
   batteryChar.coolingType = PASSIVE_COOLING;
   REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 40) == TOO_HIGH);
@@ -11,16 +11,12 @@ TEST_CASE("Test breach level for passive cooling") {
   REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 20) == NORMAL);
   REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 0) == NORMAL);
   REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 35) == NORMAL);
+  
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 50) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, -5) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 25) == NORMAL);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 45) == NORMAL);
     
 }
 
-TEST_CASE("Test breach level for active high cooling"){
-  BatteryCharacter batteryChar;
-  batteryChar.coolingType = HI_ACTIVE_COOLING;
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 50) == TOO_HIGH);
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, -1) == TOO_LOW);
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 20) == NORMAL);
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 0) == NORMAL);
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 45) == NORMAL);
-  
-}
