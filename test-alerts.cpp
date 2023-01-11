66f62658d15aafd5a86e2117ee91dd0282bf141c
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-11 09:01:39 +0530

    Update test-alerts.cpp
    Updated test cases

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -18,5 +18,26 @@ TEST_CASE("Test breach level") {
   REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 25) == NORMAL);
   REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 45) == NORMAL);
     
+  batteryChar.coolingType = MED_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 45) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, -10) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 10) == NORMAL);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 40) == NORMAL);
+   
+  batteryChar.coolingType = PASSIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 40) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, -1) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 20) == NORMAL);
+  
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 50) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, -5) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 25) == NORMAL);
+ 
+  batteryChar.coolingType = MED_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 45) == TOO_HIGH);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, -10) == TOO_LOW);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 10) == NORMAL);
+  
 }
 
