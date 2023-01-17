7d031e68bbcc0acdf1b98b86ec8a037593771fb3
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-17 21:00:36 +0530

    Update IO.c

--- IO.c
+++ IO.c
@@ -3,6 +3,9 @@
 #include "alert.h"
 #include "LanguageString.h"
 
+int AlertviaController = 0;
+int AlertviaEmail = 0;
+
 IOTarget Targettype[] ={
      {Controller,  sendToController},
      {email, sendToEmail}
@@ -15,11 +18,13 @@ void sendToIO(PrintParmsType printParms){
 void sendToController(PrintParmsType printParms) {
 
   const unsigned short header = 0xfeed;
+  AlertviaController ++;   
   printf("\n%x : %x", header, printParms.breachLvl);
 }
 
 void sendToEmail(PrintParmsType printParms) {
   const char* recepient = "a.b@c.com";
+  AlertviaEmail ++;  
   printf("\nTo: %s\nsensor value - %lf,%s,%s", recepient,
                   printParms.value,
                   stringsArray[printParms.errorMsg].LangArray[Langaugedefined],
